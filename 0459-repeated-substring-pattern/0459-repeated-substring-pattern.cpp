5728cb0e4bbf2513f5babd75c113d55943c0bbc2
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-06-29 19:42:34 +0530

    Time: 4 ms (79.82%), Space: 11.6 MB (97.89%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n = s.size();
-        for(int i = n/2; i>0; i--)
+        // int n = s.size();
+        // for(int i = n/2; i>0; i--)
+        // {
+        //     int j = 0;
+        //     while(i+j<n && s[j] == s[i+j])
+        //         j++;
+        //     if(i+j == n && s.find(s.substr(0, i-1), j) != string:: npos) return true; 
+        // }
+        // return false;
+
+        int len = s.size();
+        int size = 1;
+        while(size<=len/2)
         {
-            int j = 0;
-            while(i+j<n && s[j] == s[i+j])
-                j++;
-            if(i+j == n && s.find(s.substr(0, i-1), j) != string:: npos) return true; 
+            if(len % size != 0)
+            {
+                size++;
+                continue;
+            }
+            bool canConstruct = true;
+            for(int i = size; i<len; i++)
+            {
+                if(s[i] != s[i%size])
+                {
+                    canConstruct = false;
+                    break;
+                }
+            }
+            if(canConstruct) return true;
+            size++;
         }
         return false;
     }
