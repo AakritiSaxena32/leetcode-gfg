cfef8ff5d2f89717f612db53b96b2795de64dc1b
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-08-01 09:03:32 +0530

    Time: 99 ms (17.96%), Space: 9.5 MB (94.85%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,36 @@
+// class Solution {
+//     int score_calc(int i, int j, vector<int>&nums)
+//     {
+//         if(i>j) return 0;
+//         if(i==j) return nums[i];
+//         int take_i = nums[i] + min(score_calc(i+2, j, nums), score_calc(i+1, j-1, nums));
+//         int take_j = nums[j] + min(score_calc(i+1, j-1, nums), score_calc(i, j-2, nums));
+//         return max(take_i, take_j);
+//     }
+// public:
+//     bool predictTheWinner(vector<int>& nums) {
+//         int n = nums.size();
+//         int total_sum = accumulate(begin(nums), end(nums), 0);
+//         int p1_score = score_calc(0, n-1, nums);
+//         int p2_score = total_sum - p1_score;
+//         return (p1_score >= p2_score)? true: false;
+//     }
+// };
+
 class Solution {
     int score_calc(int i, int j, vector<int>&nums)
     {
-        if(i>j) return 0;
-        if(i==j) return nums[i];
-        int take_i = nums[i] + min(score_calc(i+2, j, nums), score_calc(i+1, j-1, nums));
-        int take_j = nums[j] + min(score_calc(i+1, j-1, nums), score_calc(i, j-2, nums));
-        return max(take_i, take_j);
+        if(i > j) return 0;
+        if(i == j) return nums[i];
+        int pick = nums[i] - score_calc(i+1, j, nums);
+        int not_pick = nums[j] - score_calc(i, j-1, nums);
+        return max(pick, not_pick);
     }
 public:
-    bool predictTheWinner(vector<int>& nums) {
+    bool predictTheWinner(vector<int>& nums) 
+    {
         int n = nums.size();
-        int total_sum = accumulate(begin(nums), end(nums), 0);
-        int p1_score = score_calc(0, n-1, nums);
-        int p2_score = total_sum - p1_score;
-        return (p1_score >= p2_score)? true: false;
+        return score_calc(0, n-1, nums) >= 0;
     }
 };
+
