db9ccca7e49d60de77b27ed4bda80df2da449a6f
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-08-30 17:41:52 +0530

    Time: 26 ms (29.75%), Space: 17.5 MB (56.75%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         int n = nums.size();
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n-3; i++)
         {
             if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1; j<n-2; j++)
