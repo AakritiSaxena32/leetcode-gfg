8f9cd46117c5163ed9650282471a5613d1c74fde
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-08-30 11:22:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (93.46%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,9 +6,9 @@ public:
         while(i<j)
         {
             if(numbers[i] + numbers[j] > target) j--;
-            else if(numbers[i] + numbers[j] < target) i--;
+            else if(numbers[i] + numbers[j] < target) i++;
             else return {i+1, j+1};
         }
-        return {-1, -1};
+        return {};
     }
 };
