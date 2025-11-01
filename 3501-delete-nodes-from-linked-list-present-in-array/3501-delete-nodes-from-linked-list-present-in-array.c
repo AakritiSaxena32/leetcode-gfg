7c403e425efc0cb8b92d240ee836ababb4b254df
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-11-01 11:01:29 +0530

    Time: 44 ms (38.46%), Space: 66.1 MB (44.62%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.c
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+#define maxN 100005
+struct ListNode* modifiedList(int* nums, int numsSize, struct ListNode* head)
+{
+    int freq[maxN]={0};
+    for(int i=0; i<numsSize; i++)
+    {
+        freq[nums[i]]++;
+    }
+    struct ListNode* dummy=(struct ListNode*)malloc(sizeof(struct ListNode));
+    dummy->next=head;
+    struct ListNode* prev=dummy;
+    struct ListNode* curr=head;
+    while(curr!=NULL)
+    {
+        if(freq[curr->val])
+        {
+            prev->next=curr->next;
+            if(curr==head)
+            {
+                head=NULL;
+            }
+            free(curr);
+            curr= prev->next;
+        }
+        else
+        {
+            prev=curr;
+            curr=curr->next;
+        }
+    }
+    struct ListNode* res=dummy->next;
+    free(dummy);
+    return res;
+}
