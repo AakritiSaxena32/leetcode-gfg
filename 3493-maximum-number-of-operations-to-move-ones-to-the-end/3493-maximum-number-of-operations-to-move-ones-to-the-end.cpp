c939f87c0bb6fc0ec87b59d3417acd38a2f95440
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-11-13 23:54:38 +0530

    Time: 10 ms (18.84%), Space: 15.8 MB (75%) - LeetHub

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int n = s.size();
+        int ones = 0;
+        int total = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(s[i] == '1') ones++;
+            else if(i>0 && s[i-1] == '1') total += ones;
+        }
+        return total;
+    }
+};
