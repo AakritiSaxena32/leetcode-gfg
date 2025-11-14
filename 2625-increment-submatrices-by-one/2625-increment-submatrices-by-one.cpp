31db5a5356166e122c881e1fbd07654e51a1f665
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-11-15 01:24:19 +0530

    Time: 4 ms (97.82%), Space: 86.4 MB (89.72%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,21 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> res(n, vector<int>(n,0));
-        for(auto query: queries)
+        // vector<vector<int>> res(n, vector<int>(n,0));
+        // for(auto query: queries)
+        // {
+        //     int stx = query[0];
+        //     int sty = query[1];
+        //     int edx = query[2];
+        //     int edy = query[3];
+        //     for(int x = stx; x <= edx; x++)
+        //     {
+        //         for(int y = sty; y <= edy; y++)
+        //         {
+        //             res[x][y] += 1; 
+        //         }
+        //     }
+        // }
+        // return res;
+
+        vector<vector<int>> res (n+1, vector<int>(n,0));
+        for(auto& q: queries)
         {
-            int stx = query[0];
-            int sty = query[1];
-            int edx = query[2];
-            int edy = query[3];
-            for(int x = stx; x <= edx; x++)
+            const int r1=q[0], c1=q[1], r2=q[2], c2=q[3];
+            res[r1][c1]++;
+            res[r2+1][c1]--;
+            if(c2+1 < n)
             {
-                for(int y = sty; y <= edy; y++)
-                {
-                    res[x][y] += 1; 
-                }
+                res[r1][c2+1]--;
+                res[r2+1][c2+1]++;
             }
         }
+        for(int i = 0; i < n; i++)
+            {
+                for(int j = 1; j < n; j++)
+                {
+                    res[i][j] += res[i][j-1];
+                }
+            }
+            for(int j = 0; j < n; j++)
+            {
+                for(int i = 1; i < n; i++)
+                {
+                    res[i][j] += res[i-1][j];
+                }
+            }
+        res.resize(n);
         return res;
     }
 };
