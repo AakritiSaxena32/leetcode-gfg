4a9b2094fd053525060541dc2965052396dbd62f
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-12-18 22:50:24 +0530

    Time: 7 ms (83.81%), Space: 316.2 MB (45.17%) - LeetHub

---
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        int n = prices.size();
+        vector<long long>profit(n, 0);
+        long long origP = 0;
+        for(int i = 0; i < n ; i++)
+        {
+            profit[i] = prices[i] * strategy[i];
+            origP += profit[i]; 
+        }
+        int i = 0, j = 0;
+        long long originalWindowProfit = 0;
+        long long modifiedWindowProfit = 0;
+        long long maxGain = 0;
+        while(j < n)
+        {
+            originalWindowProfit += profit[j];
+            if(j-i+1 > k/2)
+            {
+                modifiedWindowProfit += prices[j];
+            }
+            if(j-i+1 > k)
+            {
+                originalWindowProfit -= profit[i];
+                modifiedWindowProfit -= prices[i + k/2];
+                i++;
+            }
+            if(j-i+1 == k)
+            {
+                maxGain = max(maxGain, modifiedWindowProfit - originalWindowProfit);
+            }
+            j++;
+        }
+        return origP + maxGain;
+    }
+};
