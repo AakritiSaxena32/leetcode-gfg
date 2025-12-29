366d680d16dbf9d185e389563b7f50ec0066b424
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2025-12-30 00:17:47 +0530

    Time: 1150 ms (30.27%), Space: 157.9 MB (5.41%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -1,26 +1,30 @@
 class Solution {
+    unordered_map<string, bool> t;
     bool solve(string curr, unordered_map<string, vector<char>>& mp, int idx, string above)
     {
         if(curr.length() == 1)
         {
             return true;
         }
+        string key = curr + "_" + to_string(idx) + "_"+ above;
+        if(t.count(key))
+            return t[key];
         if(idx == curr.length()-1)
         {
-            return solve(above, mp, 0, "");
+            return t[key] = solve(above, mp, 0, "");
         }
         string pair = curr.substr(idx, 2);
         if(mp.find(pair) == mp.end())
         {
-            return false;
+            return t[key] = false;
         }
         for(char &ch: mp[pair])
         {
             above.push_back(ch);
-            if(solve(curr, mp, idx+1, above) == true) return true;
+            if(solve(curr, mp, idx+1, above) == true) return t[key] = true;
             above.pop_back();
         }
-        return false;
+        return  t[key] = false;
     }
 public:
     bool pyramidTransition(string bottom, vector<string>& allowed) {
