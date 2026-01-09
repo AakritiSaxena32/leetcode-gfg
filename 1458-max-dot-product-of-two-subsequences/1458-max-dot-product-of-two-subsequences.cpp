37d30f0510505a0ee3e9f830fe373a4a2383de69
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2026-01-09 14:03:54 +0530

    Time: 10 ms (88.98%), Space: 18 MB (32.24%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -20,25 +20,51 @@
 // };
 
 
+// class Solution {
+//     vector<vector<int>> t;
+//     int m, n;
+//     int solve(vector<int>& nums1, vector<int>& nums2, int i , int j)
+//     {
+//         if(i == n || j == m) return -1e9;
+//         if(t[i][j] != -1e9) return t[i][j];
+//         int val = nums1[i] * nums2[j];
+//         int take_ij = val + solve(nums1, nums2, i+1, j+1);
+//         int take_i = solve(nums1, nums2, i, j+1);
+//         int take_j = solve(nums1, nums2, i+1, j);
+//         return t[i][j] = max(val, max(take_ij ,max(take_i, take_j)));
+//     }
+// public:
+//         int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+//         n = nums1.size();
+//         m = nums2.size();
+//         t.resize(n, vector<int>(m, -1e9));
+//         int result = solve(nums1, nums2, 0, 0);
+//         return result;
+//     }
+// };
+
 class Solution {
-    vector<vector<int>> t;
-    int m, n;
-    int solve(vector<int>& nums1, vector<int>& nums2, int i , int j)
-    {
-        if(i == n || j == m) return -1e9;
-        if(t[i][j] != -1e9) return t[i][j];
-        int val = nums1[i] * nums2[j];
-        int take_ij = val + solve(nums1, nums2, i+1, j+1);
-        int take_i = solve(nums1, nums2, i, j+1);
-        int take_j = solve(nums1, nums2, i+1, j);
-        return t[i][j] = max(val, max(take_ij ,max(take_i, take_j)));
-    }
 public:
         int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        n = nums1.size();
-        m = nums2.size();
-        t.resize(n, vector<int>(m, -1e9));
-        int result = solve(nums1, nums2, 0, 0);
-        return result;
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1e9));
+        for(int i = n; i >= 0; i--)
+        {
+            for(int j = m; j >= 0; j--)
+            {
+                if(i == n || j == m) 
+                {
+                    dp[i][j] = -1e9;
+                    continue;
+                }
+                int val = nums1[i] * nums2[j];
+                int take_ij = val + max(0, dp[i+1][j+1]);
+                int take_i = dp[i][j+1];
+                int take_j = dp[i+1][j];
+                dp[i][j] = max(val, max(take_ij, max(take_i, take_j)));
+            }
+        }
+        return dp[0][0];
     }
 };
