7a7347371d17315d1f505e3160a839e1f67ccc59
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2026-02-09 23:36:37 +0530

    Time: 3 ms (36.73%), Space: 113.9 MB (27.32%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -7,16 +7,18 @@ public:
         while(j<n)
         {
             sum += nums[j];
-            if(j-i+1 < k)
-                j++;
-            else if(j-i+1 == k)
+            // if(j-i+1 < k)
+            //     j++;
+            // else 
+            if(j-i+1 == k)
             {
                 double avg = sum/k;
                 maxAvg = max(maxAvg, avg);
                 sum -= nums[i];
                 i++;
-                j++;
-            } 
+                // j++;
+            }
+            j++; 
         }
         return maxAvg;
     }
