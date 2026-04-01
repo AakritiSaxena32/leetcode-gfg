2d6b7d9029edb76ee7a83870ed7c7e542b9cc464
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2026-04-01 09:21:56 +0530

    Time: 8 ms (8.93%), Space: 18 MB (61.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -9,14 +9,17 @@ public:
         {
             mpp[nums[i]]++;
         }
-        priority_queue<pair<int, int>> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         for(auto & it: mpp)
         {
             pq.push({it.second, it.first});
+            if(pq.size() > k)
+            {
+                pq.pop();
+            }
         }
-        while(k-- && !pq.empty())
-        {
-            res.push_back(pq.top().second);
+        while(!pq.empty()){
+            res.push_back({pq.top().second});
             pq.pop();
         }
         return res;
