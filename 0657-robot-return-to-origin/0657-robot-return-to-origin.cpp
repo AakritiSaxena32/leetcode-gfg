3def06525ac741fb9c5ce56c9c356856d163566a
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2026-04-06 09:16:48 +0530

    Time: 0 ms (100%), Space: 10.2 MB (95.36%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int n = moves.size();
+        int u = 0, d = 0, l = 0, r= 0; 
+        for(int i = 0; i < n; i++)
+        {
+            if(moves[i] == 'U') u++;
+            else if(moves[i] == 'D') d++;
+            else if(moves[i] == 'L') l++;
+            else r++;
+        }
+        if(u != d || l != r) return false;
+        return true;
+    }
+};
