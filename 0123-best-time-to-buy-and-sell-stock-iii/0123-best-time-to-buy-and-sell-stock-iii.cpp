3421eeeaf99946c6a185192879b00b96998e9f5b
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2026-04-11 09:31:30 +0530

    Time: 7 ms (73.1%), Space: 79.3 MB (84.29%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,57 +1,76 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         // int n = prices.size();
+//         // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
+//         // for(int ind=n-1; ind>=0; ind--)
+//         // {
+//         //     for(int buy=0; buy<2; buy++)
+//         //     {
+//         //         for(int cap=1; cap<3; cap++)
+//         //         {
+//         //             if(buy == 1)
+//         //             { 
+//         //                 dp[ind][buy][cap] = max(-prices[ind]+dp[ind+1][0][cap] , dp[ind+1][1][cap]);
+//         //             }
+//         //             else
+//         //             {
+//         //                 dp[ind][buy][cap] = max(prices[ind]+dp[ind+1][1][cap-1], dp[ind+1][0][cap]);
+//         //             }
+//         //         }
+//         //     }
+//         // }
+//         // return dp[0][1][2];
+
+//         //*********************************METHOD2*************************************************
+//         // int n = prices.size();
+//         // vector<vector<int>> dp(n+1, vector<int>(5, 0));
+//         // for(int i = n-1; i>=0; i--)
+//         // {
+//         //     for(int j = 3; j>=0; j--)
+//         //     {
+//         //         if(j%2==0)
+//         //             dp[i][j] = max(-prices[i]+dp[i+1][j+1], dp[i+1][j]);
+//         //         else
+//         //             dp[i][j] = max(prices[i]+dp[i+1][j+1], dp[i+1][j]);
+//         //     }
+//         // }
+//         // return dp[0][0];
+
+//         //SPACE OBTIMIZATION    
+//         int n = prices.size();
+//         vector<int>after(5, 0);
+//         vector<int>curr(5,0);
+//         for(int i = n-1; i>=0; i--)
+//         {
+//             for(int j = 3; j>=0; j--)
+//             {
+//                 if(j%2==0)
+//                     curr[j] = max(-prices[i]+after[j+1], after[j]);
+//                 else
+//                     curr[j] = max(prices[i]+after[j+1], after[j]);
+//             }
+//             after = curr;
+//         }
+//         return after[0];
+//     }
+// };
+
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
-        // for(int ind=n-1; ind>=0; ind--)
-        // {
-        //     for(int buy=0; buy<2; buy++)
-        //     {
-        //         for(int cap=1; cap<3; cap++)
-        //         {
-        //             if(buy == 1)
-        //             { 
-        //                 dp[ind][buy][cap] = max(-prices[ind]+dp[ind+1][0][cap] , dp[ind+1][1][cap]);
-        //             }
-        //             else
-        //             {
-        //                 dp[ind][buy][cap] = max(prices[ind]+dp[ind+1][1][cap-1], dp[ind+1][0][cap]);
-        //             }
-        //         }
-        //     }
-        // }
-        // return dp[0][1][2];
-
-        //*********************************METHOD2*************************************************
-        // int n = prices.size();
-        // vector<vector<int>> dp(n+1, vector<int>(5, 0));
-        // for(int i = n-1; i>=0; i--)
-        // {
-        //     for(int j = 3; j>=0; j--)
-        //     {
-        //         if(j%2==0)
-        //             dp[i][j] = max(-prices[i]+dp[i+1][j+1], dp[i+1][j]);
-        //         else
-        //             dp[i][j] = max(prices[i]+dp[i+1][j+1], dp[i+1][j]);
-        //     }
-        // }
-        // return dp[0][0];
-
-        //SPACE OBTIMIZATION    
-        int n = prices.size();
-        vector<int>after(5, 0);
-        vector<int>curr(5,0);
-        for(int i = n-1; i>=0; i--)
+        int buy1 = INT_MIN;
+        int sell1 = 0;
+        int buy2 = INT_MIN;
+        int sell2 = 0;
+        for(int p : prices)
         {
-            for(int j = 3; j>=0; j--)
-            {
-                if(j%2==0)
-                    curr[j] = max(-prices[i]+after[j+1], after[j]);
-                else
-                    curr[j] = max(prices[i]+after[j+1], after[j]);
-            }
-            after = curr;
+            buy1 = max(buy1, -p);
+            sell1 = max(sell1, buy1 + p);
+            buy2 = max(buy2, sell1 - p);
+            sell2 = max(sell2, buy2 + p);
         }
-        return after[0];
+        return sell2;
     }
 };
