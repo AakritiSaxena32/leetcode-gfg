7e052be34500ac2b1fcf8450ba278fc550848f4a
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2026-04-11 10:10:00 +0530

    Time: 1 ms (84.48%), Space: 14.2 MB (98.72%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,19 +2,43 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) 
     {
+        // int n = prices.size();
+        // vector<int> after(2*k+1, 0), curr(2*k+1, 0);
+        // for(int ind = n-1; ind>=0; ind--)
+        // {
+        //     for(int tranNo = 2*k-1; tranNo>=0; tranNo--)
+        //     {
+        //         if(tranNo%2 == 0)
+        //             curr[tranNo] = max(-prices[ind]+after[tranNo+1], after[tranNo]);
+        //         else
+        //             curr[tranNo] = max(prices[ind]+after[tranNo+1], after[tranNo]);
+        //     }
+        //     after = curr;
+        // }
+        // return after[0]; 
+
         int n = prices.size();
-        vector<int> after(2*k+1, 0), curr(2*k+1, 0);
-        for(int ind = n-1; ind>=0; ind--)
+        if(n <= 1 || k == 0) return 0;
+        // if(k >= n/2)
+        // {
+        //     int maxProfit = 0;
+        //     for(int i = 1; i< n; i++)
+        //     {
+        //         if(prices[i] > prices[i-1]) maxProfit += max(maxProfit, prices[i]-prices[i-1]);
+        //     }
+        //     return maxProfit;
+        // }
+        vector<int>buy(k,INT_MIN);
+        vector<int>sell(k,0);
+        for(int p : prices)
         {
-            for(int tranNo = 2*k-1; tranNo>=0; tranNo--)
+            for(int j = 0; j < k; j++)
             {
-                if(tranNo%2 == 0)
-                    curr[tranNo] = max(-prices[ind]+after[tranNo+1], after[tranNo]);
-                else
-                    curr[tranNo] = max(prices[ind]+after[tranNo+1], after[tranNo]);
+                if(j == 0) buy[0] = max(buy[0], -p);
+                else buy[j] = max(buy[j], sell[j-1]-p);
+                sell[j] = max(sell[j], buy[j]+p);
             }
-            after = curr;
         }
-        return after[0]; 
+        return sell[k-1];
     }
 };
