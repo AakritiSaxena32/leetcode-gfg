5b93dbfb8da875ce90f492fe911f582ca8583284
AakritiSaxena32 <aakritisaxena2005@gmail.com>
2026-07-02 13:50:05 +0530

    Time: 116 ms (5.91%), Space: 9 MB (92.1%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,61 +1,87 @@
 class Solution {
-    void computeLPS(vector<int>& lps, string needle)
-    {
-        int length = 0;
-        lps[0] = 0;
-        int i = 1;
-        while(i<needle.size())
-        {
-            if(needle[i] == needle[length])
-            {
-                length++;
-                lps[i] = length;
-                i++;
-            }
-            else
-            {
-                if(length != 0)
-                {
-                    length = lps[length-1];
-                }
-                else
-                {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-    }
+    // void computeLPS(vector<int>& lps, string needle)
+    // {
+    //     int length = 0;
+    //     lps[0] = 0;
+    //     int i = 1;
+    //     while(i<needle.size())
+    //     {
+    //         if(needle[i] == needle[length])
+    //         {
+    //             length++;
+    //             lps[i] = length;
+    //             i++;
+    //         }
+    //         else
+    //         {
+    //             if(length != 0)
+    //             {
+    //                 length = lps[length-1];
+    //             }
+    //             else
+    //             {
+    //                 lps[i] = 0;
+    //                 i++;
+    //             }
+    //         }
+    //     }
+    // }
 public:
     int strStr(string haystack, string needle) {
+        // int n = haystack.size();
+        // int m = needle.size();
+        // int i = 0, j = 0;
+        // vector<int> lps(m);
+        // computeLPS(lps, needle);
+
+        // while(i<n)
+        // {
+        //     if(needle[j] == haystack[i])
+        //     {
+        //         i++;
+        //         j++;
+        //     }
+        //     if(j==m)
+        //     {
+        //         return i-j;
+        //     }
+        //     else if(i<n && needle[j] != haystack[i])
+        //     {
+        //         if(j!=0)
+        //         {
+        //             j = lps[j-1];
+        //         }
+        //         else
+        //         {
+        //             i++;
+        //         }
+        //     }
+        // }
+        // return -1;
+
         int n = haystack.size();
         int m = needle.size();
-        int i = 0, j = 0;
-        vector<int> lps(m);
-        computeLPS(lps, needle);
-
-        while(i<n)
+        int i = 0;
+        int j = 0;
+        while(i < n)
         {
-            if(needle[j] == haystack[i])
+            j = 0;
+            while(j < m)
             {
-                i++;
-                j++;
-            }
-            if(j==m)
-            {
-                return i-j;
-            }
-            else if(i<n && needle[j] != haystack[i])
-            {
-                if(j!=0)
+                if(haystack[i+j] == needle[j]) 
                 {
-                    j = lps[j-1];
+                    if(j == m-1)
+                    {
+                        return i;
+                    }
+                    j++;
                 }
                 else
                 {
-                    i++;
+                    break;
                 }
             }
+            i++;
         }
         return -1;
     }
